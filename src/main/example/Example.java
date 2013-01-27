3b2efb8ab8bbde203596f2764609eb46809a809c
nhibberd <nhibberd@gmail.com>
2013-01-27 19:42:51 +1000

    fix

---
+++ src/main/example/Example.java
@@ -0,0 +1,16 @@
+package example;
+
+import db.Connector;
+
+public class Example {
+
+    private static final Connector connector = new Connector("jdbc:hsqldb://localhost/test", "test", "test");
+
+    /*
+    Start with an example, with just a main method that creates a database and table, inserts some rows and queries or something.
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
